06f3310fa93d896450c04863a301d7dd00c8a712
Abouzar <abouzarmoradian1@gmail.com>
2024-08-29 11:57:48 -0400

    add login logics - check if both fields are filled

--- ApolisLoginDesignUIKit/ViewController.swift
+++ ApolisLoginDesignUIKit/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        if let username = usernameTF.text, !username.isEmpty, let password = passwordTF.text, !password.isEmpty{
+            
+        }else{
+            print("Login Error:   username and password field must be filled in order to proceed")
+        }
+    }
+    
 
 }
 
